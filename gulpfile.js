885ae2180b564ad09a2e3e5c7898fd114724d8ed
Dario Nahuel Rodriguez <darior@bandofcoders.com>
2017-08-10 22:46:20 -0300

    Add pug task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,24 @@
 'use strict';
 
-const gulp = require('gulp');
+const gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    dir = {
+        src: 'src',
+        dist: 'dist',
+        nm: 'node_modules'
+    },
+    options = {
+        pug: {
+            pretty: true,
+            locals: {
+                title: 'Single pages'
+            }
+        }
+    };
+
+gulp.task('pug', () => {
+    gulp
+        .src(`${dir.src}/pug/*.pug`)
+        .pipe(pug(options.pug))
+        .pipe(gulp.dest(`${dir.dist}/views`))
+});
